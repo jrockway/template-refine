5ed957580095f9629c890ec1b63177fea06e4176
Jonathan Rockway <jon@jrock.us>
2008-09-05 07:00:25 -0500

    add docs + version to T::R

--- lib/Template/Refine.pm
+++ lib/Template/Refine.pm
@@ -1,5 +1,17 @@
 package Template::Refine;
-use Moose;
+
+our $VERSION = '0.01';
+
 1;
 
 __END__
+
+=head1 NAME
+
+Template::Refine - refine HTML
+
+=head1 DETAILS
+
+See L<Template::Refine::Fragment>, it's currently the main entry
+point.
+
