a60255e167f5a471ce8a7b49e34a8ea433f7044b
Jonathan Rockway <jon@jrock.us>
2008-09-29 08:03:30 -0500

    make Rule lazy-buildable for subclasses

--- lib/Template/Refine/Processor/Rule.pm
+++ lib/Template/Refine/Processor/Rule.pm
@@ -2,16 +2,24 @@ package Template::Refine::Processor::Rule;
 use Moose;
 
 has 'selector' => (
-    is       => 'ro',
-    does     => 'Template::Refine::Processor::Rule::Select',
-    required => 1,
+    is      => 'ro',
+    does    => 'Template::Refine::Processor::Rule::Select',
+    lazy    => 1,
+    builder => 'build_selector',
 );
 
 has 'transformer' => ( # <insert movie reference>
-    is       => 'ro',
-    does     => 'Template::Refine::Processor::Rule::Transform',
-    required => 1,
+    is      => 'ro',
+    does    => 'Template::Refine::Processor::Rule::Transform',
+    lazy    => 1,
+    builder => 'build_transformer',
 );
+
+sub BUILD {
+    my $self = shift;
+    confess 'need a selector and transfomer'
+      unless $self->selector && $self->transformer;
+}
 
 # modifies DOM in place
 sub process {
